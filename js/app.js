f60b8c984a52484ec31ad61b56c5d643e06cde46
Mali <lindatrana06@gmail.com>
2025-07-25 20:26:07 -0600

    Realice fetch a mi bd y modifique errores al seleccionar mesa,hora y exiteAlerta.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-let mesa = {
+let cliente = {
     mesa:'',
     hora:'',
     pedidos:[]
@@ -8,14 +8,14 @@ const btnGuardar = document.querySelector('#guardar-cliente');
 btnGuardar.addEventListener('click',guargarCliente);
 
 function guargarCliente(){
-    let mesa =  document.querySelector('#mesa');
-    let hora = document.querySelector('#hora');
+    let mesa =  document.querySelector('#mesa').value;
+    let hora = document.querySelector('#hora').value;
 
     const validar = [mesa,hora].some(vacio => vacio === '');
 
     if(validar){
 
-        const existeAlerta = document.querySelector('invalid-feedback');
+        const existeAlerta = document.querySelector('.invalid-feedback');
 
         if(!existeAlerta){
             const alerta = document.createElement('DIV');
@@ -31,4 +31,27 @@ function guargarCliente(){
     }
 
     
+    cliente = {...cliente, mesa,hora};
+
+    const modalFormulario = document.querySelector('#formulario');
+    const ocultarFomulario = bootstrap.Modal.getInstance(modalFormulario);
+    ocultarFomulario.hide();
+
+    mostrarSecciones();
+    obtenerPlatillos();
 }
+
+function mostrarSecciones(){
+    const seccionesOcultas = document.querySelectorAll('.d-none');
+    seccionesOcultas.forEach(seccion => seccion.classList.remove('d-none'));
+
+}
+
+function obtenerPlatillos(){
+    const url = 'js/db.json';
+
+    fetch(url)
+                .then(datos => console.log(datos))
+                .then(resultado => console.log(resultado))
+}
+
